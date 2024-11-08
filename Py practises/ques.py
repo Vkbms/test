5946b38ced1b7878783e46c15c2e4cd5a5b9fa34
Vkbms <bibekbms20@gmail.com>
2024-11-08 22:23:14 +0530

    first

---
+++ Py practises/ques.py
@@ -0,0 +1,17 @@
+a  = 12
+b = 14
+
+#swap these 2 numbers
+
+print("Val of a before swaaping:", a)
+print("VAl of b before swapping:",b)
+
+# temp = a
+# a=b         #1st method
+# b= temp
+
+a,b=b,a  
+      #2nd method
+print("Val of a after swaaping:", a)
+print("VAl of b after swapping:",b)
+
---
+++ test.py
@@ -0,0 +1,4 @@
+import pandas as pd
+data =[2, 4, 8,15]
+series2 = pd.Series(data)
+print(series2)
